a4f08a20dd1b54d4eba07b1e348ccd7a497ea9e2
janella-may-pangilinan <janellamaypangilinan@gmail.com>
2025-02-01 14:54:50 +0800

    style

--- add_hardware.php
+++ add_hardware.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add New Hardware</title>
+   
 </head>
 <body>
     <h1>Add New Hardware</h1>
