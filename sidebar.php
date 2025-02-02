f382e09ec8b266146768911c1d079254b892ef2d
magat-airamae <magatairamaelaxa@gmail.com>
2025-02-02 15:04:05 +0800

    link logout

--- sidebar.php
+++ sidebar.php
@@ -112,7 +112,7 @@
         <li><a href="maintenance.php"><i class="fas fa-tools"></i> Maintenance</a></li>
         <li><a href="disposal.php"><i class="fas fa-trash-alt"></i> Disposal</a></li>
         <li><a href="reports.php"><i class="fas fa-chart-bar"></i> Reports</a></li>
-        <li><a href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+        <li><a href="index.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
     </ul>
 </div>
 
